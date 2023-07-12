b2548e5ccd6cfb3f1a3b52a95442b829d047caf9
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-12 14:54:28 -0700

    resolves

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,39 @@
 def badge_maker(name):
-    return None
+    sentence = f"Hello, my name is {name}."
+    return sentence
 
-def batch_badge_creator(names):
-    return None
+
+badge_maker("Rahul")
+
 
 def assign_rooms(names):
-    return None
+    index = 1
+    newlist = []
+    for name in names:
+        newlist.append(f"Hello, {name}! You'll be assigned to room {index}!")
+        index += 1
+    return newlist
+
+
+assign_rooms(["Arel", "Carol"])
+
+
+def batch_badge_creator(names):
+    badges = []
+    for name in names:
+        badge = f"Hello, my name is {name}."
+        badges.append(badge)
+    return badges
+
+
+batch_badge_creator(["Arel", "Carol"])
+
 
 def printer(names):
-    return None
+    for item in batch_badge_creator(names):
+        print(item)
+    for item in assign_rooms(names):
+        print(item)
+
+
+printer(["Arel", "Carol"])
